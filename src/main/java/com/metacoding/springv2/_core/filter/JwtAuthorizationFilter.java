58387c0bc68df3c04ebd27d837b4242068db7b4c
KimNaKim <shila66@naver.com>
2026-02-19 12:57:10 +0900

    세션 방식 로그인

--- src/main/java/com/metacoding/springv2/_core/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/metacoding/springv2/_core/filter/JwtAuthorizationFilter.java
@@ -1,7 +1,22 @@
 package com.metacoding.springv2._core.filter;
 
-// 인가 필터
-public class JwtAuthorizationFilter {
-    
- 
+import java.io.IOException;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// 인가 필터 (로그인하는 필터 아님!! -> 인가필터)
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        filterChain.doFilter(request, response);
+    }
+
 }
